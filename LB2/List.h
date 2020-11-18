c4d0f1e9a1e1ca9d92c452e5d35cbae3143a5df5
DGB35 <39164397+DGB35@users.noreply.github.com>
2020-11-19 00:39:00 +0300

    Add files via upload

---
+++ LB2/List.h
@@ -0,0 +1,39 @@
+#pragma once
+
+template <typename T, size_t N>
+class List
+{
+public:
+	List();
+
+	size_t size() const { return listSize; }
+
+	const T& operator [] (int index) const;
+	void operator += (const T&);
+
+private:
+	T list[N];
+	size_t listSize;
+};
+
+template<typename T, size_t N>
+inline List<T, N>::List(): listSize { 0 }
+{
+}
+
+template<typename T, size_t N>
+inline const T& List<T, N>::operator[](int index) const
+{
+	if (index >= N) throw std::out_of_range("Out of range");
+	return list[index];
+}
+
+template<typename T, size_t N>
+inline void List<T, N>::operator+=(const T& element)
+{
+	if (listSize < N)
+	{
+		list[listSize] = element;
+		++listSize;
+	}
+}
---
+++ LB2/Pair.h
@@ -0,0 +1,38 @@
+#pragma once
+
+template <typename A, typename B>
+class Pair
+{
+public:
+	Pair();
+	Pair(const A&, const B&);
+	const A& getKey() const;
+	const B& getValue() const;
+private:
+	A key;
+	B value;
+};
+
+template<typename A, typename B>
+inline Pair<A, B>::Pair() : key(A()), value(B())
+{
+}
+
+template<typename A, typename B>
+inline Pair<A, B>::Pair(const A& a, const B& b) : key(A()), value(B())
+{
+	key = a;
+	value = b;
+}
+
+template<typename A, typename B>
+inline const A& Pair<A, B>::getKey() const
+{
+	return key;
+}
+
+template<typename A, typename B>
+inline const B& Pair<A, B>::getValue() const
+{
+	return value;
+}
